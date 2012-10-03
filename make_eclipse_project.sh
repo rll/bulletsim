d9c8ed5b5fa66a9e9367b876e8cf5585d87ddc9f
Ibrahim Awwal <ibrahim.awwal@gmail.com>
2012-10-03 03:03:57 -0700

    Make eclipse project generator less hardcoded.

--- make_eclipse_project.sh
+++ make_eclipse_project.sh
@@ -1,4 +1,5 @@
-rm -rf ~/build/bulletsim_eclipse
-mkdir ~/build/bulletsim_eclipse
-cd ~/build/bulletsim_eclipse
-cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_PERCEPTION=ON -DJOHNS_ADVENTURES=ON ~/bulletsim
+BULLETSIM_DIR=`pwd`
+rm -rf ../build/bulletsim_eclipse
+mkdir ../build/bulletsim_eclipse
+cd ../build/bulletsim_eclipse
+cmake -G"Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DBUILD_PERCEPTION=ON -DJOHNS_ADVENTURES=ON $BULLETSIM_DIR
